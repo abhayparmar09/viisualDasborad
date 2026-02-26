2889cb4f47dc98c0045ce636c5de8e775b10aba8
abhayparmar09 <aviparmar095@gmail.com>
2026-02-27 00:10:39 +0530

    Change API URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/data';
+const API_URL = 'https://visualdasborad-backend.onrender.com';
 
 export const fetchData = async (filters = {}) => {
     const { data } = await axios.get(API_URL, { params: filters });
