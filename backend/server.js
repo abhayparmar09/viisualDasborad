3103fd490af3e1e9e292baca95e4c62ae0c20ff2
abhayparmar09 <aviparmar095@gmail.com>
2026-02-27 19:08:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -6,34 +5,24 @@ const connectDB = require('./config/db');
 const dataRoutes = require('./routes/dataRoutes');
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
-/* ---------- CORS FIX ---------- */
 app.use(cors({
-  origin: [
-    "https://visualdasborad-frontend.onrender.com/"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: "https://visualdasborad-frontend-c1gl.onrender.com/",
+  methods: ["GET","POST","PUT","DELETE"],
   credentials: true
 }));
 
 app.use(express.json());
 
-/* ---------- DB CONNECT ---------- */
-connectDB();
-
-/* ---------- ROUTES ---------- */
-app.get("/", (req,res)=>{
-  res.send("API Running Successfully 🚀");
-});
-
 app.use('/api/data', dataRoutes);
 
-/* ---------- SERVER ---------- */
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req,res)=>{
+  res.send("API running 🚀");
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
